50017a91db9e296d36d1538984e2b89f63c2cca7
Ismael Tovar <73203513+ismaeltovar@users.noreply.github.com>
2021-05-29 15:51:24 -0500

    Improved website UI & added notification alert

---
+++ notification.js
@@ -0,0 +1,4 @@
+function hello() {
+    let notification = 'This websites is in development. If you encounter a bug or if you have any suggestions, please create an issue on Github located at:\n https://github.com/IsmaelT123/ismaelt123.github.io/issues ';
+    alert(notification);
+}
