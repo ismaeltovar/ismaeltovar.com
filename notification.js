3895c310a12093e33366bc3067daa5a1719aab33
Ismael Tovar <73203513+ismaeltovar@users.noreply.github.com>
2021-06-17 07:58:29 -0500

    Fixed navigation bar & header in all pages

--- notification.js
+++ notification.js
@@ -1,4 +1,4 @@
 function getNotification() {
-    let notification = 'This websites is in development. If you encounter a bug or if you have any suggestions, please create an issue on Github located at:\n https://github.com/IsmaelT123/ismaelt123.github.io/issues ';
+    let notification = 'This websites is in development. If you encounter a bug or if you have any suggestions, please create an issue on Github located at:\n https://github.com/ismaeltovar/ismaeltovar.github.io/issues ';
     alert(notification);
 }
