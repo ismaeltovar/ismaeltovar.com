6dce723685fdb0f24802c6f94239faafdae8cf2d
Ismael Tovar <73203513+ismaeltovar@users.noreply.github.com>
2021-07-01 18:45:12 -0500

    Changed the design of the footer & moved footer code into a template in script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+const template = document.createElement('template');
+template.innerHTML = `
+  <footer>
+      <section class="socialMenu">
+        <h4>Y'all can follow me on:</h4>
+        <a class="socialButton" href="https://github.com/ismaeltovar" target="_blank">
+          <img src="./icons/tabler-icon-brand-github.svg" alt="Github Icon">
+        </a>
+        <a class="socialButton" href="#">
+          <img src="./icons/tabler-icon-brand-medium.svg" alt="Medium Icon">
+        </a>
+        <a class="socialButton" href="#" target="_blank">
+          <img src="./icons/tabler-icon-messages.svg" alt="Quora Icon">
+        </a>
+        <a class="socialButton" href="#" target="_blank">
+          <img src="./icons/tabler-icon-brand-instagram.svg" alt="Instagram Icon">
+        </a>
+      </section>
+    </footer>
+`
+document.body.appendChild(template.content);
